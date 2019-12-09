0c496c44d800c326980e20eaddbb45e4a57df259
kamalsroor <teko.2013@gmail.com>
2019-12-09 18:23:37 +0000

    test

--- app/Http/Controllers/Api/V1/UserApiController.php
+++ app/Http/Controllers/Api/V1/UserApiController.php
@@ -82,4 +82,69 @@ class UserApiController extends Controller
         }
     }
 
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function SearchSeed(Request $request)
+    {
+
+        if (!\Auth::check()) {
+            return Response::json(['error' => 'unauthenticated'], 404);
+        }
+
+        for ($i = 0; $i < count($request->seet); $i++) {
+            $dataChairs = [
+                'id' => $request->id,
+                'chair' => $request->seet[$i],
+            ];
+            $Chairs = GetRequestApi('POST', 'http://tarekmohamedreda-001-site1.atempurl.com/api/TripData/CheckCurrentSeat', $dataChairs);
+            if ($Chairs == "Not Available") {
+                return Response::json(['error' => 'الكورسي رقم ' . $request->seet[$i] . 'محجوز من قبل'], 404);
+            }
+        }
+        $data = Session::get('data');
+        $Bus = GetRequestApi('POST', 'http://tarekmohamedreda-001-site1.atempurl.com/api/TripData/SearchTrips', $data);
+        $Bus = collect($Bus);
+        $Bu = $Bus->where('id', $request->id)->first();
+        $getRandomString = rand(1000000, 9999999);
+        $totlePrice = count($request->seet) * $Bu->TripPrice;
+        $Reservation = new Reservation;
+        $Reservation->user_id = Auth::user()->id;
+        $Reservation->ref = $getRandomString;
+        $Reservation->total_price = $totlePrice;
+        $Reservation->trip_sub_data_id = $request->id;
+        $Reservation->trip_data_id = $Bu->tripdata_id;
+        $Reservation->save();
+        for ($i = 0; $i < count($request->seet); $i++) {
+            $dataChairs = [
+                'ActionEmp_Code' => 9999,
+                'ActionNode_Code' => 9999,
+                'ActionOffice_Code' => 9999,
+                'tripsubdataid' => $request->id, //id trip
+                'tripdataid' => $request->tripdata_id,
+                'SeatNo' => $request->seet[$i],
+                'Web_Ref_ID' => $getRandomString,
+                'Web_User_ID' => Auth::user()->id,
+                'Web_User_Name' => Auth::user()->name,
+            ];
+            $dataChairschesk = [
+                'id' => $request->id,
+                'chair' => $request->seet[$i],
+            ];
+            $Chairs = GetRequestApi('POST', 'http://tarekmohamedreda-001-site1.atempurl.com/api/TripData/CheckCurrentSeat', $dataChairschesk);
+            if ($Chairs == "available") {
+                $Chairs = GetRequestApi('POST', 'http://tarekmohamedreda-001-site1.atempurl.com/api/TripData/SaveSeat', $dataChairs);
+                $ReservationsInformation = new ReservationsInformation;
+                $ReservationsInformation->char_num = $request->seet[$i];
+                $ReservationsInformation->char_id = $Chairs->ID;
+                $Reservation->ReservationsInformation()->save($ReservationsInformation);
+            }
+
+        }
+        return Response::json(['success' => 'تم الحجز بنجاح' , 'id' => $Reservation->id], 200);
+    }
+
 }
